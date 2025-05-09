3a6148b79fb8e3896c5c2f4a807fb6e8199165be
EduardoRodriguezMoralesFI <eduardo.rodriguez.morales.999@gmail.com>
2025-05-09 11:01:16 -0600

    Agregar archivos de proyecto.

---
+++ ARREGLOS/Arreglos.cs
@@ -0,0 +1,37 @@
+﻿//declaracion
+//int[] numeros = int[5];
+
+string[] names = { "Hola", "Adios", "Mundo" };
+
+//Matriz arragle de 2D
+
+int[,] matriz2d = new int[2, 2];
+int[,] matriz2d2 = new int[3, 2] { { 1, 2 }, { 4, 5 }, { 7, 8 } };
+
+//int[,,] matriz2d3 = { {1, 2, 3}, {4,5,6} {7,8,9} };
+
+//acceso a los elementos
+int valor = matriz2d2[2, 1];
+
+Console.WriteLine(valor);//8
+
+//Arreglo de arreglos jagged Array 
+
+int[][] jaggedArray = new int[3][];
+
+//poblando 
+jaggedArray[0] = new int[] { 1, 2 };
+jaggedArray[1] = new int[] { 3, 4, 5 };
+jaggedArray[2] = new int[] { 6 };
+
+//Recorrecorrer el arreglo con doble for
+for (int i = 0; i < jaggedArray.Length; i++)
+{
+    Console.Write("Fila " + i + " : ");
+    for (int j = 0; j < jaggedArray[i].Length; j++)
+    {
+        Console.Write(jaggedArray[i][j] + " ");
+    }
+    Console.WriteLine();
+}
+//lista dinamica el aareglo n
---
+++ ARREGLOS/Pilas.cs
@@ -0,0 +1,26 @@
+﻿//Creacion 
+
+Stack<string> pila = new Stack<string>();
+
+//Aplicar push
+
+pila.Push("Primero");
+pila.Push("Segundo");
+pila.Push("Tercero");
+
+//Desapilar Pop
+
+string ultimo = pila.Pop();
+Console.WriteLine(ultimo); //Tercero
+
+//Desapilar peek
+ultimo = pila.Pop();
+Console.WriteLine(ultimo);
+string superior = pila.Peek();
+Console.WriteLine(superior); //Segundo
+superior = pila.Peek();
+Console.WriteLine(superior); //Segundo
+
+//Pop lo saca el elemento y lo elimina de la pila
+//el peek muestra el elemento inmediato sin eliminarlo 
+//Primero que entra es el primero que sale 
