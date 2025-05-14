bbaab6a912f7e9ca2d4afe3d3f112d5183aba623
EduardoRodriguezMoralesFI <eduardo.rodriguez.morales.999@gmail.com>
2025-05-14 10:57:57 -0600

    xd

---
+++ ARREGLOS/Diccionario.cs
@@ -0,0 +1,40 @@
+﻿//Diccionario lista enlazada
+
+Dictionary<string, int> edades = new Dictionary<string, int>();
+
+//Agregar elementos
+
+edades.Add("Ana", 25);
+edades.Add("Juan", 30);
+
+edades["Maria"] = 28;
+
+//Acceso
+
+int edadAna = edades["Ana"];
+
+//Modificar
+
+edades["Juan"] = 31;
+
+if (edades.ContainsKey("Ana"))
+{
+    Console.WriteLine("Ana existe ene este diccionario");
+}
+
+if (edades.TryGetValue("Pedro", out int edadPedro))
+{
+    Console.WriteLine($"Si existe pedro con la edad: {edadPedro}");
+}
+else
+{
+    Console.WriteLine("Pedro no esta registrado");
+}
+
+//Recorrer la memoria no es contigua, ocpan difrente espacio de memoria 
+
+foreach (KeyValuePair<string, int> parDiccionario in edades)
+{
+    Console.WriteLine($"{parDiccionario.Key} : {parDiccionario.Value}");
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿LinkedList<string> frutas = new LinkedList<string>();
+
+//Agregar nodos 
+
+frutas.AddFirst("Platano");
+frutas.AddLast("Mango");
+frutas.AddFirst("Pera");
+
+//Insertar elementos en medio 
+
+LinkedListNode<string> nodoPlatano = frutas.Find("Platano");
+
+frutas.AddBefore(nodoPlatano, "Sandia");
+
+//Eliminar nodos 
+
+frutas.Remove("Mango");
+
+LinkedListNode<string> actual = frutas.First;
+
+while (actual != null)
+{
+    string anterior = actual.Previous?.Value ?? "null";
+    string siguiente = actual.Next?.Value ?? "null";
+    Console.WriteLine($"{anterior} <- {actual.Value} -> {siguiente}");
+    actual = actual.Next;
+
+}
+
